0a24d3b3f33e1f3a2ac46deaa012ac0d632db922
Cassie Andrews <candrews1@ufl.edu>
2025-08-04 19:48:28 -0400

    might need to normalize data to fix the rendering issue, pause

--- components/plantList/index.jsx
+++ components/plantList/index.jsx
@@ -5,13 +5,21 @@ import styles from "./style.module.css"
 export default function PlantList({ plants }) {
     return (
         <div className={styles.list}>
-            {plants.map((plant) => (
-                <Link
-                    key={plant.id} 
-                    href={`/plant/${plant.id}`}>
-                    <PlantCard plant={plant} />
-                </Link>
-            ))}
+            {plants.map((plant) => {
+                const key = plant.plant_id || plant.id || (plant._id ? plant._id.toString() : null)
+                const href = `/plant/${plant.plant_id || plant.id || ""}`
+
+                if (!key) {
+                    return null
+                }
+                return (
+                    <Link
+                        key={key} 
+                        href={href}>
+                        <PlantCard plant={plant} />
+                    </Link>
+                )
+            })}
         </div>
     )
 }
