ec472996ed6b1c8c84389210af3aafc5d40eb188
Cassie Andrews <candrews1@ufl.edu>
2025-08-04 19:05:32 -0400

    troubleshooting collection page, Error serializing  returned from

--- db/controllers/plant.js
+++ db/controllers/plant.js
@@ -5,16 +5,32 @@ export async function getAll(userId) {
     await dbConnect()
     const user = await User.findById(userId).lean()
     if (!user) return null
-    return user.plantCollection.map(plant => plant)
+
+
+    const cleanedCollection = user.plantCollection.map(plant => {
+        return {
+        ...plant,
+        _id: plant._id.toString(),
+        }   
+    })
+    return cleanedCollection
 }
 
 export async function getByPlantId(userId, plantId) {
     await dbConnect()
     const user = await User.findById(userId).lean()
     if (!user) return null
-    const plant = user.plantCollection.find(plant => plant.plantId === plantId)
-    if (plant) return
-    return null
+
+    const plant = user.plantCollection.find((plant) => plant.plant_Id === plantId)
+    if (plant) return {
+        ...plant,
+        _id: plant._id.toString(),
+    }
 }
 
-
+export async function getCollection(userId) {
+    await dbConnect()
+    const user = await User.findById(userId).lean()
+    if (!user) return null
+    return user.plantCollection || []
+}
--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ import * as user from './controllers/user'
 import * as auth from './controllers/auth'
 import * as plant from './controllers/plant'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   user,
   auth,
